4e9d20cc7d8d3756101951297f7522dd090044c1
Quinn Collins <collinsqui@gmail.com>
2017-06-01 20:35:55 -0400

    Added Dollar Amount and Main
    This was a test

--- src/main/java/net/qcollins/Main.java
+++ src/main/java/net/qcollins/Main.java
@@ -1,5 +1,8 @@
 package net.qcollins;
 
 public class Main {
-
+//	private static DollarAmount price = new DollarAmount(Integer.valueOf("3.05"));
+	public static void main(String[] args) {
+		System.out.println(Integer.valueOf("305"));
+	}
 }
